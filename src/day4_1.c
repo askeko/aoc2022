64dd77ffd6331a235561b7194e7e1dcf3bf88d4a
askeko <askeklitgaard@gmail.com>
2022-12-04 11:18:54 +0100

    solve day 4 part 1

---
+++ src/day4_1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_OF_PAIRS 1000
+
+typedef struct cleaner_pair_t {
+    int elf1_low;
+    int elf1_up;
+    int elf2_low;
+    int elf2_up;
+} cleaner_pair_t;
+
+void open_file(FILE* fp);
+void scan_cleaners(FILE* fp, cleaner_pair_t* cleaners);
+int check_same_range(cleaner_pair_t* cleaners);
+
+int main() {
+    FILE* fp = fopen("day4_cleaners.txt", "r");
+    if (fp == NULL) {
+        printf("Cannot open file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cleaner_pair_t* cleaners = malloc(sizeof(cleaner_pair_t) * NUM_OF_PAIRS);
+    if (cleaners == NULL) {
+        printf("Could not find cleaners\n");
+        exit(EXIT_FAILURE);
+    }
+
+    scan_cleaners(fp, cleaners);
+    int contains_same_range = check_same_range(cleaners);
+    printf("%d assignments contain the same range", contains_same_range);
+
+    fclose(fp);
+    free(cleaners);
+
+    return 0;
+}
+
+void scan_cleaners(FILE* fp, cleaner_pair_t* cleaners) {
+    for (int i = 0; i < NUM_OF_PAIRS; i++) {
+        fscanf(fp, "%d-%d,%d-%d", &cleaners[i].elf1_low, &cleaners[i].elf1_up,
+               &cleaners[i].elf2_low, &cleaners[i].elf2_up);
+    }
+}
+
+int check_same_range(cleaner_pair_t* cleaners) {
+    int contains_same_range = 0;
+    for (int i = 0; i < NUM_OF_PAIRS; i++) {
+        if (cleaners[i].elf1_low >= cleaners[i].elf2_low &&
+            cleaners[i].elf1_up <= cleaners[i].elf2_up) {
+            contains_same_range++;
+        } else if (cleaners[i].elf2_low >= cleaners[i].elf1_low &&
+                   cleaners[i].elf2_up <= cleaners[i].elf1_up) {
+            contains_same_range++;
+        }
+    }
+
+    return contains_same_range;
+}
